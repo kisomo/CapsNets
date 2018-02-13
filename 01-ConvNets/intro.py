09418ccf14d59ac2045e09ad9cc89f0211bcd8b1
kisomo <tmuthoka@gmail.com>
2018-02-12 22:32:18 -0600

    updated the LeNet5

--- 01-ConvNets/intro.py
+++ 01-ConvNets/intro.py
@@ -54,8 +54,8 @@ print("There are {} images, each of size {}".format(len(mnist_train_dataset), le
 print("Meaning each image has the size of 28*28*1 = {}".format(mnist_image_width*mnist_image_height*1))
 print("The training set contains the following {} labels: {}".format(len(np.unique(mnist_train_labels_)), np.unique(mnist_train_labels_)))
  
-print('Training set shape', mnist_train_dataset.shape, mnist_train_labels.shape)
-print('Test set shape', mnist_test_dataset.shape, mnist_test_labels.shape)
+print('MNIST Training set shape', mnist_train_dataset.shape, mnist_train_labels.shape)
+print('MNIST Test set shape', mnist_test_dataset.shape, mnist_test_labels.shape)
  
 train_dataset_mnist, train_labels_mnist = mnist_train_dataset, mnist_train_labels
 test_dataset_mnist, test_labels_mnist = mnist_test_dataset, mnist_test_labels
@@ -94,13 +94,13 @@ train_dataset_cifar10, train_labels_cifar10 = reformat_data(c10_train_dataset, c
 del c10_train_dataset
 del c10_train_labels
  
-print("The training set contains the following labels: {}".format(np.unique(c10_train_dict[b'labels'])))
-print('Training set shape', train_dataset_cifar10.shape, train_labels_cifar10.shape)
-print('Test set shape', test_dataset_cifar10.shape, test_labels_cifar10.shape)
+print("CIFAR The training set contains the following labels: {}".format(np.unique(c10_train_dict[b'labels'])))
+print('CIFAR Training set shape', train_dataset_cifar10.shape, train_labels_cifar10.shape)
+print('CIFAR Test set shape', test_dataset_cifar10.shape, test_labels_cifar10.shape)
 
 
 
-# fit a 1 layer FCNN
+# +++++++++++++++++++fit a 1 layer FCNN to mnist dataset  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 image_width = mnist_image_width
 image_height = mnist_image_height
@@ -115,19 +115,9 @@ test_dataset = mnist_test_dataset
 test_labels = mnist_test_labels 
 
 
-'''
-train_dataset = 10
-train_labels = 10
-test_dataset = 10
-test_labels = 10
-
-#mn = prepare_data("mnist")
-mnist = data_prep.prepare_data("mnist",train_dataset,train_labels,test_dataset,test_labels)
-mnist = prepare_data._display(mnist)
 #number of iterations and learning rate
-'''
-num_steps = 1001  #10001
-display_step = 100  #1000
+num_steps = 101  #10001
+display_step = 10  #1000
 learning_rate = 0.5
 
  
@@ -141,7 +131,7 @@ with graph.as_default():
     #2) Then, the weight matrices and bias vectors are initialized
     #as a default, tf.truncated_normal() is used for the weight matrix and tf.zeros() is used for the bias vector.
     weights = tf.Variable(tf.truncated_normal([image_width * image_height * image_depth, num_labels]), tf.float32)
-    bias = tf.Variable(tf.zeros([num_labels]), tf.float32)
+    bias = tf.Variable(tf.ones([num_labels]), tf.float32)
   
     #3) define the model:
     #A one layered fccd simply consists of a matrix multiplication
@@ -166,7 +156,15 @@ with tf.Session(graph=graph) as session:
     tf.global_variables_initializer().run()
     print('Initialized')
     for step in range(num_steps):
-        _, l, predictions = session.run([optimizer, loss, train_prediction])
+        #Since we are using stochastic gradient descent, we are selecting  small batches from the training dataset,
+        #and training the convolutional neural network each time with a batch. 
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        #_, l, predictions = session.run([optimizer, loss, train_prediction])
         if (step % display_step == 0):
             train_accuracy = accuracy(predictions, train_labels[:, :])
             test_accuracy = accuracy(test_prediction.eval(), test_labels)
@@ -174,7 +172,196 @@ with tf.Session(graph=graph) as session:
             print(message)
 
 
+# +++++++++++++++++++fit a 1 layer FCNN to CIFAR10 dataset  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+image_width = c10_image_width
+image_height = c10_image_height
+image_depth = c10_image_depth
+num_labels = c10_num_labels 
+batch_size = 10 # I just created this
+
+#the dataset
+train_dataset = train_dataset_cifar10
+train_labels = train_labels_cifar10 
+test_dataset = test_dataset_cifar10
+test_labels = test_labels_cifar10
+
+
+#number of iterations and learning rate
+num_steps = 101  #10001
+display_step = 10  #1000
+learning_rate = 0.5
+
+ 
+graph = tf.Graph()
+with graph.as_default():
+    #1) First we put the input data in a tensorflow friendly form. 
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_width, image_height, image_depth))
+    tf_train_labels = tf.placeholder(tf.float32, shape = (batch_size, num_labels))
+    tf_test_dataset = tf.constant(test_dataset, tf.float32)
+  
+    #2) Then, the weight matrices and bias vectors are initialized
+    #as a default, tf.truncated_normal() is used for the weight matrix and tf.zeros() is used for the bias vector.
+    weights = tf.Variable(tf.truncated_normal([image_width * image_height * image_depth, num_labels]), tf.float32)
+    bias = tf.Variable(tf.ones([num_labels]), tf.float32)
+  
+    #3) define the model:
+    #A one layered fccd simply consists of a matrix multiplication
+    def model(data, weights, bias):
+        return tf.matmul(flatten_tf_array(data), weights) + bias
+        #return tf.nn.xw_plus_b(train_dataset, weights, bias)  # Terrence exchanged this
+ 
+    logits = model(tf_train_dataset, weights, bias)
+    #4) calculate the loss, which will be used in the optimization of the weights
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
+ 
+    #5) Choose an optimizer. Many are available.
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+ 
+    #6) The predicted values for the images in the train dataset and test dataset are assigned to the variables train_prediction and test_prediction. 
+    #It is only necessary if you want to know the accuracy by comparing it with the actual values. 
+    train_prediction = tf.nn.softmax(logits)
+    test_prediction = tf.nn.softmax(model(tf_test_dataset, weights, bias))
+ 
+
+with tf.Session(graph=graph) as session:
+    tf.global_variables_initializer().run()
+    print('Initialized')
+    for step in range(num_steps):
+        #Since we are using stochastic gradient descent, we are selecting  small batches from the training dataset,
+        #and training the convolutional neural network each time with a batch. 
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        #_, l, predictions = session.run([optimizer, loss, train_prediction])
+        if (step % display_step == 0):
+            train_accuracy = accuracy(predictions, train_labels[:, :])
+            test_accuracy = accuracy(test_prediction.eval(), test_labels)
+            message = "step {:04d} : loss is {:06.2f}, accuracy on training set {:02.2f} %, accuracy on test set {:02.2f} %".format(step, l, train_accuracy, test_accuracy)
+            print(message)
+
+
+#+++++++++++++++++++++ leNet5 on mnist +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+LENET5_BATCH_SIZE = 32
+LENET5_PATCH_SIZE = 5
+LENET5_PATCH_DEPTH_1 = 6
+LENET5_PATCH_DEPTH_2 = 16
+LENET5_NUM_HIDDEN_1 = 120
+LENET5_NUM_HIDDEN_2 = 84
+ 
+def variables_lenet5(patch_size = LENET5_PATCH_SIZE, patch_depth1 = LENET5_PATCH_DEPTH_1, 
+                     patch_depth2 = LENET5_PATCH_DEPTH_2, 
+                     num_hidden1 = LENET5_NUM_HIDDEN_1, num_hidden2 = LENET5_NUM_HIDDEN_2,
+                     image_depth = 1, num_labels = 10):
+    
+    w1 = tf.Variable(tf.truncated_normal([patch_size, patch_size, image_depth, patch_depth1], stddev=0.1))
+    b1 = tf.Variable(tf.zeros([patch_depth1]))
+ 
+    w2 = tf.Variable(tf.truncated_normal([patch_size, patch_size, patch_depth1, patch_depth2], stddev=0.1))
+    b2 = tf.Variable(tf.constant(1.0, shape=[patch_depth2]))
+ 
+    w3 = tf.Variable(tf.truncated_normal([5*5*patch_depth2, num_hidden1], stddev=0.1))
+    b3 = tf.Variable(tf.constant(1.0, shape = [num_hidden1]))
+ 
+    w4 = tf.Variable(tf.truncated_normal([num_hidden1, num_hidden2], stddev=0.1))
+    b4 = tf.Variable(tf.constant(1.0, shape = [num_hidden2]))
+    
+    w5 = tf.Variable(tf.truncated_normal([num_hidden2, num_labels], stddev=0.1))
+    b5 = tf.Variable(tf.constant(1.0, shape = [num_labels]))
+    variables = {
+        'w1': w1, 'w2': w2, 'w3': w3, 'w4': w4, 'w5': w5,
+        'b1': b1, 'b2': b2, 'b3': b3, 'b4': b4, 'b5': b5
+    }
+    return variables
+ 
+def model_lenet5(data, variables):
+    layer1_conv = tf.nn.conv2d(data, variables['w1'], [1, 1, 1, 1], padding='SAME')
+    layer1_actv = tf.sigmoid(layer1_conv + variables['b1'])
+    layer1_pool = tf.nn.avg_pool(layer1_actv, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
+ 
+    layer2_conv = tf.nn.conv2d(layer1_pool, variables['w2'], [1, 1, 1, 1], padding='VALID')
+    layer2_actv = tf.sigmoid(layer2_conv + variables['b2'])
+    layer2_pool = tf.nn.avg_pool(layer2_actv, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
+ 
+    flat_layer = flatten_tf_array(layer2_pool)
+    layer3_fccd = tf.matmul(flat_layer, variables['w3']) + variables['b3']
+    layer3_actv = tf.nn.sigmoid(layer3_fccd)
+
+    layer4_fccd = tf.matmul(layer3_actv, variables['w4']) + variables['b4']
+    layer4_actv = tf.nn.sigmoid(layer4_fccd)
+    logits = tf.matmul(layer4_actv, variables['w5']) + variables['b5']
+    return logits
+
+
+#parameters determining the model size
+#image_size = mnist_image_size
+#num_labels = mnist_num_labels
+
+image_width = mnist_image_width
+image_height = mnist_image_height
+image_depth = mnist_image_depth
+num_labels = mnist_num_labels 
+batch_size = 10 # I just created this
+
+
+#the datasets
+train_dataset = mnist_train_dataset
+train_labels = mnist_train_labels
+test_dataset = mnist_test_dataset
+test_labels = mnist_test_labels
+ 
+#number of iterations and learning rate
+num_steps = 1001
+display_step = 100
+learning_rate = 0.5 #0.001
+ 
+graph = tf.Graph()
+with graph.as_default():
+    #1) First we put the input data in a tensorflow friendly form. 
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_width, image_height, image_depth))
+    tf_train_labels = tf.placeholder(tf.float32, shape = (batch_size, num_labels))
+    tf_test_dataset = tf.constant(test_dataset, tf.float32)
+ 
+    #2) Then, the weight matrices and bias vectors are initialized
+    variables = variables_lenet5(image_depth = image_depth, num_labels = num_labels)
+ 
+    #3. The model used to calculate the logits (predicted labels)
+    model = model_lenet5
+    logits = model(tf_train_dataset, variables)
+ 
+    #4. then we compute the softmax cross entropy between the logits and the (actual) labels
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
+    
+    #5. The optimizer is used to calculate the gradients of the loss function 
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+ 
+    # Predictions for the training, validation, and test data.
+    train_prediction = tf.nn.softmax(logits)
+    test_prediction = tf.nn.softmax(model(tf_test_dataset, variables))
+
+
+with tf.Session(graph=graph) as session:
+    tf.global_variables_initializer().run()
+    print('Initialized with learning_rate', learning_rate)
+    for step in range(num_steps):
+ 
+        #Since we are using stochastic gradient descent, we are selecting  small batches from the training dataset,
+        #and training the convolutional neural network each time with a batch. 
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        
+        if step % display_step == 0:
+            train_accuracy = accuracy(predictions, batch_labels)
+            test_accuracy = accuracy(test_prediction.eval(), test_labels)
+            message = "step {:04d} : loss is {:06.2f}, accuracy on training set {:02.2f} %, accuracy on test set {:02.2f} %".format(step, l, train_accuracy, test_accuracy)
+            print(message)
 
 
 
