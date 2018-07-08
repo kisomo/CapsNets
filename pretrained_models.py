ec6e89323815db2be09e55982dd9bba6da421a60
kisomo <tmuthoka@gmail.com>
2018-07-07 23:39:05 -0500

    still on pre-trained models

--- pretrained_models.py
+++ pretrained_models.py
@@ -267,7 +267,7 @@ score = log_loss(Y_valid, predictions_valid)
 
 #https://machinelearningmastery.com/use-pre-trained-vgg-model-classify-objects-photographs/
 
-
+'''
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
@@ -297,8 +297,493 @@ label = label[0][0]
 # print the classification
 print('%s (%.2f%%)' % (label[1], label[2]*100))
 
+'''
+#http://cv-tricks.com/tensorflow-tutorial/training-convolutional-neural-network-for-image-classification/
+
+'''
+import cv2
+import os
+import glob
+from sklearn.utils import shuffle
+import numpy as np
+
+
+def load_train(train_path, image_size, classes):
+    images = []
+    labels = []
+    img_names = []
+    cls = []
+
+    print('Going to read training images')
+    for fields in classes:   
+        index = classes.index(fields)
+        print('Now going to read {} files (Index: {})'.format(fields, index))
+        path = os.path.join(train_path, fields, '*g')
+        files = glob.glob(path)
+        for fl in files:
+            image = cv2.imread(fl)
+            image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
+            image = image.astype(np.float32)
+            image = np.multiply(image, 1.0 / 255.0)
+            images.append(image)
+            label = np.zeros(len(classes))
+            label[index] = 1.0
+            labels.append(label)
+            flbase = os.path.basename(fl)
+            img_names.append(flbase)
+            cls.append(fields)
+    images = np.array(images)
+    labels = np.array(labels)
+    img_names = np.array(img_names)
+    cls = np.array(cls)
+
+    return images, labels, img_names, cls
+
+
+class DataSet(object):
+    def __init__(self, images, labels, img_names, cls):
+        self._num_examples = images.shape[0]
+
+        self._images = images
+        self._labels = labels
+        self._img_names = img_names
+        self._cls = cls
+        self._epochs_done = 0
+        self._index_in_epoch = 0
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+       return self._labels
+
+    @property
+    def img_names(self):
+        return self._img_names
+
+    @property
+    def cls(self):
+       return self._cls
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_done(self):
+        return self._epochs_done
+
+    def next_batch(self, batch_size):
+        """Return the next `batch_size` examples from this data set."""
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+           # After each epoch we update this
+           self._epochs_done += 1
+           start = 0
+           self._index_in_epoch = batch_size
+           assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+        return self._images[start:end], self._labels[start:end], self._img_names[start:end], self._cls[start:end]
+
+
+def read_train_sets(train_path, image_size, classes, validation_size):
+  class DataSets(object):
+    pass
+  data_sets = DataSets()
+
+  images, labels, img_names, cls = load_train(train_path, image_size, classes)
+  images, labels, img_names, cls = shuffle(images, labels, img_names, cls)  
+
+  if isinstance(validation_size, float):
+     validation_size = int(validation_size * images.shape[0])
+
+  validation_images = images[:validation_size]
+  validation_labels = labels[:validation_size]
+  validation_img_names = img_names[:validation_size]
+  validation_cls = cls[:validation_size]
+
+  train_images = images[validation_size:]
+  train_labels = labels[validation_size:]
+  train_img_names = img_names[validation_size:]
+  train_cls = cls[validation_size:]
+
+  data_sets.train = DataSet(train_images, train_labels, train_img_names, train_cls)
+  data_sets.valid = DataSet(validation_images, validation_labels, validation_img_names, validation_cls)
+
+  return data_sets
+
+
+
+#-------start tensorflow -----------
+import tensorflow as tf
+
+a = tf.truncated_normal([16,128,128,3])
+sess = tf.Session()
+#sess.run(tf.initialize_all_variables())
+sess.run(tf.global_variables_initializer())
+sess.run(tf.shape(a))
+
+b=tf.reshape(a,[16,49152])
+sess.run(tf.shape(b))
+
+classes = ['dogs', 'cats']
+num_classes = len(classes)
+ 
+train_path='/home/terrence/CODING/Python/MODELS/dogs_and_cats/train/'
+#test_path='/home/terrence/CODING/Python/MODELS/dogs_and_cats/test1/'
+ 
+# validation split
+validation_size = 0.2
+ 
+# batch size
+batch_size = 16
+
+dataset = Dataset()
+data = dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
+
+def create_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+ 
+def create_biases(size):
+    return tf.Variable(tf.constant(0.05, shape=[size]))
+
+
+def create_convolutional_layer(input,
+               num_input_channels, 
+               conv_filter_size,        
+               num_filters):  
+    
+    ## We shall define the weights that will be trained using create_weights function.
+    weights = create_weights(shape=[conv_filter_size, conv_filter_size, num_input_channels, num_filters])
+    ## We create biases using the create_biases function. These are also trained.
+    biases = create_biases(num_filters)
+ 
+    ## Creating the convolutional layer
+    layer = tf.nn.conv2d(input=input,
+                     filter=weights,
+                     strides=[1, 1, 1, 1],
+                     padding='SAME')
+ 
+    layer += biases
+ 
+    ## We shall be using max-pooling.  
+    layer = tf.nn.max_pool(value=layer,
+                            ksize=[1, 2, 2, 1],
+                            strides=[1, 2, 2, 1],
+                            padding='SAME')
+    ## Output of pooling is fed to Relu which is the activation function for us.
+    layer = tf.nn.relu(layer)
+ 
+    return layer
+
+
+def create_flatten_layer(layer):
+    layer_shape = layer.get_shape()
+    num_features = layer_shape[1:4].num_elements()
+    layer = tf.reshape(layer, [-1, num_features])
+ 
+    return layer
+
+
+def create_fc_layer(input,          
+             num_inputs,    
+             num_outputs,
+             use_relu=True):
+    
+    #Let's define trainable weights and biases.
+    weights = create_weights(shape=[num_inputs, num_outputs])
+    biases = create_biases(num_outputs)
+ 
+    layer = tf.matmul(input, weights) + biases
+    if use_relu:
+        layer = tf.nn.relu(layer)
+ 
+    return layer
+
+x = tf.placeholder(tf.float32, shape=[None, img_size,img_size,num_channels], name='x')
+ 
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+
+layer_conv1 = create_convolutional_layer(input=x,
+               num_input_channels=num_channels,
+               conv_filter_size=filter_size_conv1,
+               num_filters=num_filters_conv1)
+ 
+layer_conv2 = create_convolutional_layer(input=layer_conv1,
+               num_input_channels=num_filters_conv1,
+               conv_filter_size=filter_size_conv2,
+               num_filters=num_filters_conv2)
+ 
+layer_conv3= create_convolutional_layer(input=layer_conv2,
+               num_input_channels=num_filters_conv2,
+               conv_filter_size=filter_size_conv3,
+               num_filters=num_filters_conv3)
+          
+layer_flat = create_flatten_layer(layer_conv3)
+ 
+layer_fc1 = create_fc_layer(input=layer_flat,
+                     num_inputs=layer_flat.get_shape()[1:4].num_elements(),
+                     num_outputs=fc_layer_size,
+                     use_relu=True)
+ 
+layer_fc2 = create_fc_layer(input=layer_fc1,
+                     num_inputs=fc_layer_size,
+                     num_outputs=num_classes,
+                     use_relu=False)
+
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
+                                                    labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+
+batch_size = 16
+ 
+x_batch, y_true_batch, _, cls_batch = data.train.next_batch(batch_size)
+ 
+feed_dict_train = {x: x_batch,
+                           y_true: y_true_batch}
+ 
+session.run(optimizer, feed_dict=feed_dict_tr)
+
+x_valid_batch, y_valid_batch, _, valid_cls_batch = data.valid.next_batch(train_batch_size)
+ 
+feed_dict_val = {x: x_valid_batch,
+                      y_true: y_valid_batch}
+ 
+val_loss = session.run(cost, feed_dict=feed_dict_val)
+
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver.save(session, 'dogs-cats-model')
+
+
+def train(num_iteration):
+    global total_iterations
+    
+    for i in range(total_iterations,
+                   total_iterations + num_iteration):
+ 
+        x_batch, y_true_batch, _, cls_batch = data.train.next_batch(batch_size)
+        x_valid_batch, y_valid_batch, _, valid_cls_batch = data.valid.next_batch(batch_size)
+ 
+        
+        feed_dict_tr = {x: x_batch,
+                           y_true: y_true_batch}
+        feed_dict_val = {x: x_valid_batch,
+                              y_true: y_valid_batch}
+ 
+        session.run(optimizer, feed_dict=feed_dict_tr)
+ 
+        if i % int(data.train.num_examples/batch_size) == 0: 
+            val_loss = session.run(cost, feed_dict=feed_dict_val)
+            epoch = int(i / int(data.train.num_examples/batch_size))    
+            
+            show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
+            saver.save(session, 'dogs-cats-model') 
+ 
+ 
+    total_iterations += num_iteration
+
+saver = tf.train.import_meta_graph('flowers-model.meta')
+
+saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+image = cv2.imread(filename)
+# Resizing the image to our desired size and
+# preprocessing will be done exactly as done during training
+image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
+images.append(image)
+images = np.array(images, dtype=np.uint8)
+images = images.astype('float32')
+images = np.multiply(images, 1.0/255.0) 
+#The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
+x_batch = images.reshape(1, image_size,image_size,num_channels)
+ 
+ 
+graph = tf.get_default_graph()
+ 
+y_pred = graph.get_tensor_by_name("y_pred:0")
+ 
+## Let's feed the images to the input placeholders
+x= graph.get_tensor_by_name("x:0") 
+y_true = graph.get_tensor_by_name("y_true:0") 
+y_test_images = np.zeros((1, 2)) 
+ 
+feed_dict_testing = {x: x_batch, y_true: y_test_images}
+result=sess.run(y_pred, feed_dict=feed_dict_testing)
+
+python predict.py test_dog.jpg
+#[[ 0.99398661  0.00601341]]
+
+'''
 
 #http://cv-tricks.com/keras/fine-tuning-tensorflow/
+
+img_size=224
+ap = argparse.ArgumentParser()
+ap.add_argument("-train","--train_dir",type=str, required=True,help="(required) the train data directory")
+ap.add_argument("-num_class","--class",type=int, required=True,help="(required) number of classes to be trained")
+ap.add_argument("-val","--val_dir",type=str, required=True,help="(required) the validation data directory")
+
+batch_size=10
+ 
+train_datagen = image.ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+ 
+test_datagen = image.ImageDataGenerator(rescale=1. / 255)
+train_generator = train_datagen.flow_from_directory(
+        args["train_dir"],
+        target_size=(img_size, img_size),
+        batch_size=batch_size,
+        class_mode='categorical')
+ 
+validation_generator = test_datagen.flow_from_directory(
+        args["val_dir"],
+        target_size=(img_size,img_size),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+
+ # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+ 
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+ 
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+ 
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+ 
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+ 
+    if include_top:
+        # Classification block
+        x = Flatten(name='flatten')(x)
+        x = Dense(4096, activation='relu', name='fc1')(x)
+        x = Dense(4096, activation='relu', name='fc2')(x)
+        x = Dense(classes, activation='softmax', name='predictions')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = GlobalMaxPooling2D()(x)
+
+
+if weights == 'imagenet':
+        if include_top:
+            weights_path = get_file('vgg16_weights_tf_dim_ordering_tf_kernels.h5',
+                                    WEIGHTS_PATH,
+                                    cache_subdir='models',
+                                    file_hash='64373286793e3c8b2b4e3219cbf3544b')
+        else:
+            weights_path = get_file('vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                    WEIGHTS_PATH_NO_TOP,
+                                    cache_subdir='models',
+                                    file_hash='6d6bbae143d832006294945121d1f1fc')
+        model.load_weights(weights_path)
+
+
+base_model = VGG16.VGG16(include_top=False, weights='imagenet')
+
+
+i=0
+for layer in base_model.layers:
+    layer.trainable = False
+    i = i+1
+    print(i,layer.name)
+
+x = base_model.output
+x = Dense(128, activation='sigmoid')(x)
+x = GlobalAveragePooling2D()(x)
+x = Dropout(0.2)(x)
+predictions = Dense(args["class"], activation='softmax')(x)
+
+pip install tensorboard
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+
+tensorboard --logdir=logs  ## Speficy the path which you want to read
+
+filepath = 'cv-tricks_fine_tuned_model.h5'
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,save_best_only=True,save_weights_only=False, mode='auto',period=1)
+callbacks_list = [checkpoint,tensorboard]
+
+model = Model(inputs=base_model.input, outputs=predictions)
+ 
+model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=0.001, momentum=0.9),metrics=["accuracy"])
+ 
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=100,
+        epochs=10,
+        callbacks = callbacks_list,
+        validation_data = validation_generator,
+        validation_steps=20
+        )
+
+
+python 1_vgg16_pretrain.py  -train ../tutorial-2-image-classifier/training_data/ -val ../tutorial-2-image-classifier/testing_data/ -num_class 2
+
+tensorboard --logdir=logs
+
+# We pass weights=None since we don't want any weights here
+base_model = VGG16.VGG16(include_top=False, weights=None)
+
+# After creating the network
+model.load_weights("cv-tricks_pretrained_model.h5")
+
+model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),metrics=["accuracy"])
+
+
+base_model = VGG16.VGG16(include_top=False, weights=None)
+x = base_model.output
+x = Dense(128)(x)
+x = GlobalAveragePooling2D()(x)
+predictions = Dense(args["class"], activation='softmax')(x)
+ 
+model = Model(inputs=base_model.input, outputs=predictions)
+ 
+model.load_weights("cv-tricks_fine_tuned_model.h5")
+ 
+inputShape = (224,224) # Assumes 3 channel image
+image = load_img(args["image"], target_size=inputShape)
+image = img_to_array(image)   # shape is (224,224,3)
+image = np.expand_dims(image, axis=0)  # Now shape is (1,224,224,3)
+ 
+image = image/255.0
+ 
+preds = model.predict(image)
+print(preds)
+
+python 3_predict.py --image test.jpg
+#[[  2.86891848e-13   1.00000000e+00]]
+
+
 
 
 
